85ef2b27ba62ab4dea923cd2c700606d27824e4a
Ivan Virabyan <i.virabyan@gmail.com>
2014-08-27 16:05:39 +0400

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-mentions',
-    version='0.1.3',
+    version='0.1.4',
     author='Ivan Virabyan',
     packages=find_packages(),
     description='Facebook-like mentions for Django',
