b302d39eadcbc31c2016c67e79cda24a437b65e7
Anton Solovyh <bazz@kanobu.ru>
2014-09-04 12:46:51 +0400

    Release 1.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-mentions',
-    version='0.1.7',
+    version='0.1.8',
     author='Ivan Virabyan',
     packages=find_packages(),
     description='Facebook-like mentions for Django',
